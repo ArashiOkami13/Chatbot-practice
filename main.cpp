7f1724264e09d75768f79ab3468b3a7e6b3c9981
raul.fosselman <raul.fosselman@navy.mil>
2020-06-12 15:58:13 -0700

    running, but with weird bugs. no outer for loop to make debugging easier

--- main.cpp
+++ main.cpp
@@ -1,50 +1,48 @@
 #include <unistd.h>
 #include <semaphore.h>
+#include <pthread.h>
 #include <iostream>
 #include <fstream>
 #define NUM_THREADS     7
 std::ofstream myFile;
-std::string evenQuote = "Controlling complexity is the essence of computer programming.--Brian Kernigan";
-std::string oddQuote = "Computer science is no more about computers than astronomy is about telescopes --Edsger Dijkstra";
+std::string evenQuote = "Controlling complexity is the essence of computer programming.--Brian Kernigan\n";
+std::string oddQuote = "Computer science is no more about computers than astronomy is about telescopes --Edsger Dijkstra\n";
 sem_t FLAG;
 
-void * printHello(void * threadID){
-    for(int i = 0; i < NUM_THREADS; i++){
-        long tid = 2;
-        // long tid = (long) threadID;
-        std::cout << tid;
-        sem_wait(&FLAG); // Decrement, lock the semaphore
-        myFile.open("quote.txt", std::ios::ate);
-        if( tid % 2 == 0){
-            // sleep(2);
-            myFile << "Thread " << threadID << " is  running " << evenQuote << "\r\n";
-        }
-        else{
-            // sleep(3);
-            myFile << "Thread " << threadID << " is  running " << evenQuote << "\r\n";
-        }
-        myFile.close();
-        sem_post(&FLAG); //increment, unlock the semaphore
+void * printHello(void * something){
+    sem_wait(&FLAG);
+
+    std::cout << "printing " << (long) something << "\n";
+    myFile.open("quote.txt", std::ios_base::app);
+    if( (long) something % 2 == 0){
+        // sleep(2);
+        myFile << "Thread " << std::to_string( (long) something ) << " is running " << evenQuote << "\r\n";
     }
+    else{
+        // sleep(3);
+        myFile << "Thread " << std::to_string( (long) something ) << " is running " << oddQuote << "\r\n";
+    }
+    myFile.close();
+    sem_post(&FLAG);
     pthread_exit(NULL);
 }
 
 int main (int argc, char *argv[]) {
     //Open the txt file, write to it and close it
     myFile.open ("quote.txt");
-    myFile << getpid() <<"\r\n";
+    myFile << std::to_string( getpid() ) <<"\r\n";
     myFile.close();
 
     //Initialize Semaphore to 1
     sem_init(&FLAG, 0, 1);
     
     pthread_t threads[NUM_THREADS];
-    std::cout<< "do we lock here?";
+
     for(int i = 0; i < NUM_THREADS; i++){
-        int returnCode = pthread_create(&threads[i], NULL, &printHello, (void *)i);
+        int returnCode = pthread_create(&threads[i], NULL, &printHello, (void *) i);
         if(returnCode) std::cout << "error";
+        std::cout << "nothing interesting " << "\n";
     }
 
-    pthread_exit(NULL);
     return 0;
 }
