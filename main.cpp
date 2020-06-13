c51c9c889205dbb6afc2a5d7da53e2b214cf00f6
raul <raul@localhost.localdomain>
2020-06-13 12:58:17 -0700

    working version w/ comments

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+/*************************************************************
+Name: Raul Fosselman, William Nelson
+Username: cssc2104
+Class: CS570 Summer 2020
+Project: CS-570 Assignment 1
+File: main.cpp
+Desc: This file encompasses the entire project, upon running 7
+threads write to the file quote.txt safely with the use of semaphores
+*************************************************************/
+
 #include <unistd.h>
 #include <semaphore.h>
 #include <pthread.h>
@@ -9,28 +19,36 @@ std::string evenQuote = "Controlling complexity is the essence of computer progr
 std::string oddQuote = "Computer science is no more about computers than astronomy is about telescopes --Edsger Dijkstra\n";
 sem_t FLAG;
 
-void * printHello(void * something){
-    sem_wait(&FLAG);
+void * printHello(void * tid){
+    //Let each thread print their output 8 times
+    for (int i = 0; i < 9; i++)
+    {
+        //Sleep 2 or 3 seconds
+        sleep(2 + ((long) tid % 2));
 
-    std::cout << "printing " << (long) something << "\n";
-    myFile.open("quote.txt", std::ios_base::app);
-    if( (long) something % 2 == 0){
-        // sleep(2);
-        myFile << "Thread " << std::to_string( (long) something ) << " is running " << evenQuote << "\r\n";
+        //Decrement semaphore (lock)
+        sem_wait(&FLAG);
+        
+        //Open file and append a quote to it
+        myFile.open("quote.txt", std::ios_base::app);
+        if( (long) tid % 2 == 0){
+            myFile << "Thread " <<  (long) tid << " is running " << evenQuote << "\r\n";
+        }
+        else{
+            myFile << "Thread " << (long) tid << " is running " << oddQuote << "\r\n";
+        }
+        myFile.close();
+        //Increment semaphore (unlock)
+        sem_post(&FLAG);
     }
-    else{
-        // sleep(3);
-        myFile << "Thread " << std::to_string( (long) something ) << " is running " << oddQuote << "\r\n";
-    }
-    myFile.close();
-    sem_post(&FLAG);
-    pthread_exit(NULL);
 }
 
 int main (int argc, char *argv[]) {
-    //Open the txt file, write to it and close it
+    std::cout << "Running ... " << "\n";
+
+    //Open the txt file, write the Process ID to it and close it
     myFile.open ("quote.txt");
-    myFile << std::to_string( getpid() ) <<"\r\n";
+    myFile << "process id: " << getpid() << "\r\n";
     myFile.close();
 
     //Initialize Semaphore to 1
@@ -38,11 +56,18 @@ int main (int argc, char *argv[]) {
     
     pthread_t threads[NUM_THREADS];
 
-    for(int i = 0; i < NUM_THREADS; i++){
+    //Create the threads
+    for(int i = 0; i <= NUM_THREADS; i++){
         int returnCode = pthread_create(&threads[i], NULL, &printHello, (void *) i);
-        if(returnCode) std::cout << "error";
-        std::cout << "nothing interesting " << "\n";
+        if(returnCode) exit(1);
     }
+    //Join threads so they dont get killed by the main  thread before they finish
+    for (int i = 0; i <= NUM_THREADS; i++)
+       pthread_join(threads[i], NULL);
+
+    sem_destroy(&FLAG);
+
+    std::cout << "Finished, check quote.txt for output " << "\n";
 
     return 0;
 }
